bd2c385460c8ff6bc5debfdc584bbc99c50bb8b5
Ahmed Raza <raza1778@gmail.com>
2018-08-10 21:17:27 +0500

    Registered chat-compomnent

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,6 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.component('chat-component', require('./components/ChatComponent.vue'));
 Vue.component('articles', require('./components/Articles.vue'));
 
 const app = new Vue({
