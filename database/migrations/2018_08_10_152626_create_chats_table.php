f1c16f024a3f4bf86c3e293a27b7c19bbbe012f0
Ahmed Raza <raza1778@gmail.com>
2018-08-10 21:21:14 +0500

    migrations of chats and message along with chat_user pivot table

---
+++ database/migrations/2018_08_10_152626_create_chats_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+        Schema::create('chat_user', function (Blueprint $table) {
+            $table->integer('chat_id')->unsigned()->index();
+            $table->foreign('chat_id')->references('id')->on('chats')->onDelete('cascade');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_user');
+        Schema::dropIfExists('chats');
+    }
+}
