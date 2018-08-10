d37d1d2cb5dae2ef6966140a540aa442de15cdf6
Ahmed Raza <raza1778@gmail.com>
2018-08-10 21:16:58 +0500

    Added relationship with App\Chat

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Chat;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function chats() {
+        return $this->belongsToMany(Chat::class);
+    }
 }
