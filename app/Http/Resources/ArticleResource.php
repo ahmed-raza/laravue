26631fcffb88fbf5a71d29f6b48e2577c482849e
Alex Roye <alex@creativefaze.com>
2019-01-25 17:50:07 +0500

    Sending custom response

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -14,6 +14,11 @@ class ArticleResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'        => $this->id,
+            'title'     => $this->title,
+            'body'      => $this->body,
+            'created_at'  => $this->created_at->diffForHumans(),
+        ];
     }
 }
